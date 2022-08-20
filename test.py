74551e5ba0099dadb70dc5de2eb85d5c45602ed2
fake31818 <111655344+fake31818@users.noreply.github.com>
2022-08-20 22:06:29 +0430

    Add files via upload

---
+++ test.py
@@ -0,0 +1,4 @@
+import os
+
+os.system("npm install torrent")
+os.system("npx torrent magnet:?xt=urn:btih:3184ac6deff8510251547cd7791016def21079b8&dn=Lupin.S02.FRENCH.720p.NF.WEBRip.DDP5.1.x264-AGLET%5Brartv%5D")
