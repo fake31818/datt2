d00154fb6f0dd93944dd87a6c5e00f9176fcadaf
fake31818 <111655344+fake31818@users.noreply.github.com>
2022-08-23 01:22:45 +0430

    Update py.py

--- py.py
+++ py.py
@@ -61,28 +61,20 @@ def tup(name_file):
     sys.stdout.write('\r'+ "Loading... 4/4")
     driver.find_element(by=By.ID,value="f_upload").send_keys(patchfile + "/" + name_file)
     WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div[2]/div/div[1]/ul/li/div/p[2]/span")))
-    number = driver.find_elements(by=By.XPATH,value="/html/body/div[1]/div[3]/div/div[2]/div[2]/div/div[1]/ul/li/div/p[2]/span")
     print("\n" + name_file)
     num = True
     while num == True:
-        for elt in number:
-            x = elt.text
-            xu = x.replace("%", "")
-            elt2 = int(xu)
-            if(elt2 > 10 and elt2 < 12):print(elt.text)
-            if(elt2 > 25 and elt2 < 27):print(elt.text)
-            if(elt2 > 50 and elt2 < 55):print(elt.text)
-            if(elt2 > 75 and elt2 < 77):print(elt.text)
-            if(elt2 > 90 and elt2 < 95):print(elt.text)
-            if(elt2 > 99):
-                num = False
-        time.sleep(0.1)
+        try:
+            WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div[2]/div/div[1]/ul/li")))
+            break
+        except:
+            pass
+         
     time.sleep(1)
-    print("100%")	
-    WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div[2]/div/div[1]/ul/li")))
-    time.sleep(8)
+    print("100%")
+    time.sleep(5)
     print("Upload Done! :D")
-
+    driver.close()
 
 if(option_url == "2"):
     driver.get(url_file)
@@ -93,5 +85,4 @@ if(url_mega_1 != "no"):
 if(url_mega_2 != "no"):
     driver.get(url_mega_2)
     mega_name()
-driver.close()
 print("------------")
