17f0950244d1113b32275b12cf2a9d62ff67779f
fake31818 <111655344+fake31818@users.noreply.github.com>
2022-12-24 19:08:45 +0330

    Create UTB.py

---
+++ UTB.py
@@ -0,0 +1,114 @@
+import json
+import logging
+import os
+import pathlib
+import sys
+import threading
+from typing import Optional
+try:import boto3
+except:
+    os.system("apt update;apt install python3-pip -y;pip install -q boto3;clear")
+    import boto3
+from boto3.s3.transfer import TransferConfig
+from botocore.exceptions import ClientError
+
+# Constant variables
+KB = 1024
+MB = KB * KB
+GB = MB * KB
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# S3 client instance
+s3_client = boto3.client(
+    's3',
+    endpoint_url='https://s3.ir-tbz-sh1.arvanstorage.com/',
+    aws_access_key_id='5600e4a9-b2bf-4c42-9243-dedd20f7fa67',
+    aws_secret_access_key='577b54dbe1f2b8e2b3b64bc1630f02faf1d39088'
+)
+
+class ProgressPercentage:
+    def __init__(self, file_path: str):
+        self._file_path = file_path
+        self._size = float(os.path.getsize(file_path))
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+
+    def __call__(self, bytes_amount):
+        """
+        To simplify, assume this is hooked up to a single file_path
+
+        :param bytes_amount: uploaded bytes
+        """
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            sys.stdout.write(
+                "\r%s  %s / %s  (%.2f%%)" % (self._file_path, self._seen_so_far, self._size, percentage)
+            )
+            sys.stdout.flush()
+
+
+def upload_file(file_path: str, bucket: str, object_name: Optional[str] = None):
+    """
+    Upload a file to an S3 bucket
+
+    :param file_path: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_path is used
+    :return: True if file was uploaded, else False
+    """
+    # If S3 object_name was not specified, use file_path
+    if object_name is None:
+        object_name = file_path
+
+    # Upload the file
+    try:
+        # Set the desired multipart threshold value (400 MB)
+        config = TransferConfig(multipart_threshold=400 * MB, max_concurrency=5)
+        s3_client.upload_file(
+            file_path,
+            bucket,
+            object_name,
+            ExtraArgs={'ACL': 'public-read'},
+            Callback=ProgressPercentage(file_path),
+            Config=config
+        )
+    except ClientError as e:
+        logging.error(e)
+        return False
+
+    return True
+option = os.environ['option']
+url1 = os.environ['url1']
+url2 = os.environ['url2']
+url3 = os.environ['url3']
+url = [url1]
+object_name1 = os.environ['name1']
+object_name2 = os.environ['name2']
+object_name3 = os.environ['name3']
+object_names = [object_name1]
+if object_name2 != "no":
+    object_names.append(object_name2)
+    url.append(url2)
+if object_name3 != "no":
+    object_names.append(object_name3)
+    url.append(url3)
+i = 0
+for url in url:
+    object_name = object_names[i]
+    if option == 1:
+        os.system(f"curl '{url}' -o '{object_name}'")
+    elif option == 2:
+        os.system(f"mega-get '{url}'")
+    else:
+      print("Error 204")
+      exit()
+    print("\n"+20*"="+"\n")
+    print("\n"+object_name)
+    base_directory="./"
+    file_rel_path: str = os.path.join('', object_name)
+    file_abs_path: str = os.path.join(base_directory, file_rel_path)
+    upload_file(file_abs_path, 'arvs', object_name)
+    i+=1
